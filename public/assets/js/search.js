5b9e035b04bbdd413eb4b153655d7883b2b594d9
BrianHdz <popcorn46290@sbcglobal.net>
2020-03-03 00:07:55 -0800

    public/assets/js/search.js work in progress.

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -13,11 +13,18 @@ $(document).ready(function () {
         console.log("Search button was clicked!")
         // Need to set up the api call with the api key in the header. Not sure how yet. Here is where I left off. The URL that is not commented is a placeholder. It works. The one below that IS commented out is the one we want to use.
         $.ajax({
-            url: "https://api.edamam.com/search?q=steak&app_id=00c46abe&app_key=2e68c484c12e4ddcf68cd4c939cef6e0&from=0&to=3&Health=paleo",
-            // url: "https://api.mashvisor.com/v1.1/client/city/properties/CA/Los%20Angeles",
-            method: "GET",
-            //this doesn't work below
-            //headers: "x-api-key", "0b3c70fe-d5b1-438e-8e6f-1feb2bcaf143":
+            type: "GET",
+            url: "https://api.mashvisor.com/v1.1/client/city/properties/CA/Los%20Angeles",
+            xhrFields: {withCredentials: false},
+            headers: { "x-api-key": "0b3c70fe-d5b1-438e-8e6f-1feb2bcaf143" },
+            dataType: "json",
+            data: {},
+            success: function (result) {
+                console.log(result);
+            },
+            error: function () {
+                console.log("error");
+            }
         }).then(function (response) {
             console.log(response);
         })
@@ -29,6 +36,24 @@ $(document).ready(function () {
 
 
 
+
+
+
+
+
+/*
+Testing Code
+-----------------------------------------------------------------------------------
+$.ajax({
+            // url: "https://api.edamam.com/search?q=steak&app_id=00c46abe&app_key=2e68c484c12e4ddcf68cd4c939cef6e0&from=0&to=3&Health=paleo",
+            url: "https://api.mashvisor.com/v1.1/client/city/properties/CA/Los%20Angeles",
+            method: "GET",
+            headers: {"x-api-key": "[0b3c70fe-d5b1-438e-8e6f-1feb2bcaf143]"}
+        }).then(function (response) {
+            console.log(response);
+        })
+
+        */
 
 // // Function for handling what to render when there are no authors
 // function renderEmpty() {
