63a0fcf518681ec6726bd82049c44c6fccd95156
BrianHdz <popcorn46290@sbcglobal.net>
2020-03-04 00:13:21 -0800

    Setting up db tables. Added notes in login.js

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO rentaldb.users (email, password) values ('Brian@gmail.com', '123');
+
+INSERT INTO rentaldb.properties (theState, theAddress, starred, purchased) values ('CA', 'firstPlace', TRUE, false);
+INSERT INTO rentaldb.properties (theState, theAddress, starred, purchased) values ('CA', 'secondSpot', TRUE, TRUE);
+INSERT INTO rentaldb.properties (theState, theAddress, starred, purchased) values ('CA', 'thirdLocation', false, TRUE);
+
+
+/*
+-----These are extras-----
+INSERT INTO rentaldb.users (email, password) values ('Rover@gmail.com', '456');
+INSERT INTO rentaldb.users (email, password) values ('Mott@gmail.com', '789');
+*/
