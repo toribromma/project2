b872821e747f787c19eafc97598f45008a17c4ad
Tori <torimpham@gmail.com>
2020-03-03 21:00:45 -0800

    fixed config.js file

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
         dialect: 'mysql'
     },
     production: {
-        use_env_variable: JAWSDB_URL,
-        dialect: mysql
+        use_env_variable: "JAWSDB_URL",
+        dialect: "mysql"
       }
     }
