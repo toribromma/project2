8771f5faa12c0df3d2c32284482a4e183ead9d9c
Tori <torimpham@gmail.com>
2020-03-03 18:49:25 -0800

    made login work

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,26 @@
 // Importing the model (index.js) to use it's database functions
 var index = require("../models/index.js");
 var path = require("path");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function (app) {
 
     // "practice" route loads "login.handlebars" page. Right now it shows the Cats! page (as a placeholder) but that's just a practice route to be deleted once the REAL login page is created.
-    app.get("/", function (req, res) {
-        res.render("index");
-    });
+    app.get("/", function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+          res.render("search");
+        }
+            res.render("index")
+      });
 
-    app.get("/login", function (req, res) {
+      app.get("/login", function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+          res.render("search");
+        }
         res.render("logIn");
-    });
+      });
 
     // signup route loads login page.
     app.get("/signup", function (req, res) {
@@ -21,20 +30,20 @@ module.exports = function (app) {
     });
 
      // signup route loads login page.
-     app.get("/profile", function (req, res) {
+     app.get("/profile", isAuthenticated, function (req, res) {
         res.render("profile");
     });
 
-    app.get("/investment", function (req, res) {
+    app.get("/investment", isAuthenticated, function (req, res) {
         res.render("investment");
     });
 
-    app.get("/properties", function (req, res) {
+    app.get("/properties", isAuthenticated, function (req, res) {
         res.render("properties");
     });
 
-    app.get("/search", function (req, res) {
-        res.render("search");
-    });
+    app.get("/search", isAuthenticated, function(req, res) {
+            res.render("search")
+      });
 
 };
