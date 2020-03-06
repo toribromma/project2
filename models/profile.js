a47e1ceb660e7054253166d578d15fd23cea99c8
Jason Blake <jblakecodes@gmail.com>
2020-03-05 19:48:46 -0800

    associated profile with user inside profile model

--- models/profile.js
+++ models/profile.js
@@ -28,5 +28,15 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.INT,
         }
     });
+
+    Profile.associate = function (models) {
+        // We're saying that a Post should belong to an Author
+        // A Post can't be created without an Author due to the foreign key constraint
+        Profile.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
     return Profile;
 };     
