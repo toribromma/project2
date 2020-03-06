8941c308730d5fabf74a044e29d187c5aaf3844d
Tori <torimpham@gmail.com>
2020-03-05 21:28:56 -0800

    got login and log out to work

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -7,6 +7,9 @@ $(document).ready(function() {
   // When the form is submitted, we validate there's an email and password entered
   loginForm.on("submit", function(event) {
     event.preventDefault();
+
+    $("#logout").removeClass("hide")
+    
     var userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
@@ -24,34 +27,22 @@ $(document).ready(function() {
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
   function loginUser(email, password) {
-    // $.put("/api/login", {
-    //   email: email,
-    //   password: password
-    // })
-    //   .then(function() {
-    //     window.location.replace("/search");
-    //     // If there's an error, log the error
-    //   })
-    //   .catch(function(err) {
-    //     console.log(err);
-    //   });
-
-      $.ajax({
-        method: "PUT",
-        url: "/api/signup",
-        data: {
-          email: email,
-        password: password
-        }
+    $.ajax({
+      method: "POST",
+      url: "/api/login",
+      data: {
+        email: email,
+      password: password
+      }
+    })
+      .then(function() {
+        window.location.href = "/search";
+        // If there's an error, log the error
       })
-        .then(function() {
-          window.location.href = "/search";
-          // If there's an error, log the error
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
-    }
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
   
 
 });
