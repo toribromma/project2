c0c2352956e16d3d079ec20738d3ae94878057d6
BrianHdz <popcorn46290@sbcglobal.net>
2020-03-05 21:52:01 -0800

    API Call works on the search JS files/page.

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set("view engine", "handlebars");
 // // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/searchRoutes")(app);
 
 var syncOptions = { force: false };
 
