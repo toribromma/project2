ca7d71b9eec84f9d32a4b678e2c7702908f38b9a
BrianHdz <popcorn46290@sbcglobal.net>
2020-03-07 01:38:35 -0800

    Search page works now.

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -3,20 +3,27 @@ var db = require("../models");
 var passport = require("../config/passport");
 var axios = require("axios");
 
+
+
+
 module.exports = function (app) {
     app.get("/api/search", function (req, res) {
-        axios.get("https://mashvisor-api.p.rapidapi.com/city/properties/GA/Atlanta", {
-            // "async": true,
-            // "crossDomain": true,
-            // "url": "https://mashvisor-api.p.rapidapi.com/city/properties/GA/Atlanta",
-            // "method": "GET",
+
+        var state = req.query.state
+        var city = req.query.city
+        console.log(state)
+        console.log(city)
+
+        axios.get(`https://mashvisor-api.p.rapidapi.com/city/properties/${state}/${city}`, {
+
             "headers": {
                 "x-rapidapi-host": "mashvisor-api.p.rapidapi.com",
                 "x-rapidapi-key": process.env.API_KEY
             }
         }).then(function (result) {
             res.json(result.data.content.properties);
-            console.log(result.data.content.properties[0]);
+            console.log(result.data.content.properties[0].neighborhood);
+            // console.log(``)
         })
 
     });
@@ -26,19 +33,6 @@ module.exports = function (app) {
 
 
 
-
 /*
-event.preventDefault();
-        console.log("Search button was clicked!")
-        // Need to set up the api call with the api key in the header. Not sure how yet. Here is where I left off. The URL that is not commented is a placeholder. It works. The one below that IS commented out is the one we want to use.
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://mashvisor-api.p.rapidapi.com/city/properties/GA/Atlanta",
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "mashvisor-api.p.rapidapi.com",
-                "x-rapidapi-key": "c6608f007amsh63d70b201c30d34p12279cjsn2ae84034d585"
-            }
-        }
+${state}/${city}
 */
