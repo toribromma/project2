69eef37874b09d832c9ef4144f2b6c93530cd2eb
Tori <torimpham@gmail.com>
2020-03-07 11:40:38 -0800

    added delete button to profile

--- public/assets/js/profiles.js
+++ public/assets/js/profiles.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     // var cities = $("#citiesInterested");
     // var email = $("#profile-email");
     var phone = $("#phone");
-
+    $(document).on("click", "button.delete", deleteProfile);
     $("#profileSave").on("click", function (event) {
         event.preventDefault();
         $("#profileDiv").empty();
@@ -24,10 +24,12 @@ $.post("/api/profile", Profile)
         .then(function() {
 
             var row = $("<div>");
-            row.append("<p>" + Profile.firstName + "</p>");
-            row.append("<p>" + Profile.lastName + "</p>");
-            row.append("<p>" + Profile.budget + "</p>");
-            row.append("<p>" + Profile.phone + "</p>");
+            row.data(Profile.id)
+            row.append("<p>" + "First Name: " + Profile.firstName + "</p>");
+            row.append("<p>" + "Last Name: " + Profile.lastName + "</p>");
+            row.append("<p>" + "Budget: " + Profile.budget + "</p>");
+            row.append("<p>" + "Phone Number: " + Profile.phone + "</p>");
+            row.append("<button class='delete btn btn-danger'>x</button>")
             $("#profileDiv").prepend(row);
         });
     });
@@ -46,11 +48,31 @@ if (data.length !== 0) {
             row.append("<p>" + data[i].lastName + "</p>");
             row.append("<p>" + data[i].budget + "</p>");
             row.append("<p>" + data[i].phone + "</p>");
-
+            row.append("<button class='delete btn btn-danger'>x</button>")
             $("#profileDiv").prepend(row);
     }
         }
 });
+
+
+  // This function deletes a todo when the user clicks the delete button
+  function deleteProfile(event) {
+    event.stopPropagation();
+    var id = $(this).data("id");
+    $.ajax({
+      method: "DELETE",
+      url: "/api/profile/" + id
+    }).then(window.location.reload);
+  }
+
+
+
+
+
+
+
+
+
 });
 
 
