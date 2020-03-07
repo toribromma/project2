3beaf1fb35e7c71007c784a9269cff9666514754
Tori <torimpham@gmail.com>
2020-03-07 01:26:25 -0800

    working file with additions to profile and minor detail changes

--- public/assets/js/profiles.js
+++ public/assets/js/profiles.js
@@ -1,25 +1,56 @@
+
 $(document).ready(function () {
 
     var firstName = $("#firstName");
     var lastName = $("#lastName");
     var budget = $("#budget");
-    var cities = $("#citiesInterested");
-    var email = $("#profile-email");
+    // var cities = $("#citiesInterested");
+    // var email = $("#profile-email");
     var phone = $("#phone");
 
     $("#profileSave").on("click", function (event) {
         event.preventDefault();
-        var userData = {
+        var Profile = {
             firstName: firstName.val().trim(),
             lastName: lastName.val().trim(),
             budget: budget.val().trim(),
-            citiesInterested: cities.val().trim(),
-            phone: phone.val().trim(),
-            email: email.val().trim()
+            phone: phone.val().trim(), 
         };
+
+        console.log(Profile);
+
+$.post("/api/profile", Profile)
+        .then(function() {
+
+            var row = $("<div>");
+            row.append("<p>" + Profile.firstName + "</p>");
+            row.append("<p>" + Profile.lastName + "</p>");
+            row.append("<p>" + Profile.budget + "</p>");
+            row.append("<p>" + Profile.phone + "</p>");
+            $("#profileDiv").prepend(row);
+        });
     });
 
+$.get("/api/profile", function(data) {
+
+if (data.length !== 0) {
+
+        for (var i = 0; i < data.length; i++) {
+
+            var row = $("<div>");
+
+            row.append("<p>" + data[i].firstName + "</p>");
+            row.append("<p>" + data[i].lastName + "</p>");
+            row.append("<p>" + data[i].budget + "</p>");
+            row.append("<p>" + data[i].phone + "</p>");
+
+            $("#profileDiv").prepend(row);
+    }
+        }
 });
+});
+
+
 
 
 
